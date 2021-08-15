1a8e95696c4f8df4cf982b53a6601e56ae1c87a0
Andrew Bui <andrew.bui.au@gmail.com>
2021-08-15 16:11:45 +1000

    initialised synthRoll repo

---
+++ synthRoll_iOS/synthRoll_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  synthRoll_iOS
+//
+//  Created by Andrew Bui on 15/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
